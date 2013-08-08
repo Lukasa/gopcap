37306a525d80530792fdc8c77e4bfb8fb75adb95
Cory Benfield <lukasaoz@gmail.com>
2013-08-08 21:28:52 +0100

    Some parsing utility functions.

--- api.go
+++ api.go
@@ -7,6 +7,8 @@ functionality in as clear an API as possible.
 package gopcap
 
 import (
+	"bytes"
+	"errors"
 	"io"
 	"time"
 )
@@ -120,4 +122,75 @@ type Packet struct {
 // produces .pcap files. It will attempt to parse the entire file. If an error
 // is encountered, as much of the parsed content as is possible will be returned,
 // along with an error value.
-func Parse(src io.Reader) (PcapFile, error) {}
+func Parse(src io.Reader) (PcapFile, error) {
+	file := new(PcapFile)
+
+	// Check whether this is a libpcap file at all, and if so what byte ordering it has.
+	cont, flipped, err := checkMagicNum(src)
+	if err != nil {
+		return *file, err
+	}
+
+	return *file, nil
+}
+
+// checkMagicNum checks the first four bytes of a pcap file, searching for the magic number
+// and checking the byte order. Returns three values: whether the file is a pcap file, whether
+// the byte order needs flipping, and any error that was encountered. If error is returned,
+// the other values are invalid.
+func checkMagicNum(src io.Reader) (bool, bool, error) {
+	// These magic numbers form the header of a pcap file.
+	magic := []byte{0xa1, 0xb2, 0xc3, 0xd4}
+	magic_reverse := []byte{0xd4, 0xc3, 0xb2, 0xa1}
+
+	buffer := make([]byte, 4)
+	read_count, err := src.Read(buffer)
+
+	if err != nil {
+		return false, false, err
+	}
+	if read_count != 4 {
+		return false, false, errors.New("Insufficent length.")
+	}
+
+	if bytes.Compare(buffer, magic) == 0 {
+		return true, false, nil
+	} else if bytes.Compare(buffer, magic_reverse) == 0 {
+		return true, true, nil
+	}
+
+	return false, false, errors.New("Not a pcap file.")
+}
+
+// populateFileHeader reads the next 20 bytes out of the .pcap file and uses it to populate the
+// PcapFile structure.
+func populateFileHeader(file *PcapFile, src io.Reader, flipped bool) error {
+	buffer := make([]byte, 20)
+	read_count, err := src.Read(buffer)
+
+	if err != nil {
+		return err
+	} else if read_count != 20 {
+		return errors.New("Insufficient length.")
+	}
+
+	// First two bytes are the major version number.
+	file.MajorVersion = getUint16(buffer[0:2], flipped)
+
+	// Next two are the minor version number.
+	file.MinorVersion = getUint16(buffer[2:4], flipped)
+
+	// GMT to local correction, in seconds east of UTC.
+	file.TZCorrection = getInt32(buffer[4:8], flipped)
+
+	// Next is the number of significant figures in the timestamps. Almost always zero.
+	file.SigFigs = getUint32(buffer[8:12], flipped)
+
+	// Now the maximum length of the captured packet data.
+	file.MaxLen = getUint32(buffer[12:16], flipped)
+
+	// And the link type.
+	file.LinkType = Link(getUint32(buffer[16:20], flipped))
+
+	return nil
+}
