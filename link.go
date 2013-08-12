612b865ce2afd66d2b0cefb0610c8d867202b512
Cory Benfield <lukasaoz@gmail.com>
2013-08-12 21:14:35 +0100

    Define the Ethernet frame type for now.

---
+++ link.go
@@ -0,0 +1,15 @@
+package gopcap
+
+// EthernetFrame represents a single ethernet frame. Valid only when the LinkType is ETHERNET.
+type EthernetFrame struct {
+	MACSource      [6]byte
+	MACDestination [6]byte
+	VLANTag        uint32
+	Length         uint16
+	EtherType      uint16
+	data           []byte
+}
+
+func (e EthernetFrame) Data() []byte {
+	return data
+}
